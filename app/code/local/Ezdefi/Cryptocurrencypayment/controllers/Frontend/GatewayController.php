c8475d1d4dd6b7158ac2e2bfb5ea1e7969a0994b
blackskin <khanhpham08121996@gmail.com>
2020-05-21 15:06:38 +0700

    remove vardump

--- app/code/local/Ezdefi/Cryptocurrencypayment/controllers/Frontend/GatewayController.php
+++ app/code/local/Ezdefi/Cryptocurrencypayment/controllers/Frontend/GatewayController.php
@@ -15,7 +15,6 @@ class Ezdefi_Cryptocurrencypayment_Frontend_GatewayController extends Mage_Core_
         $publicKeyStatus = Mage::helper('cryptocurrencypayment/GatewayApi')->checkPublicKey($requests['public_key'], $requests['api_key'], $requests['gateway_api_url']);
 
         if($publicKeyStatus == 'true') {
-            var_dump(1);
             Mage::helper('cryptocurrencypayment/GatewayApi')->updateCallbackUrl('ezdefi_frontend/callback/confirmorder', $requests['public_key'], $requests['api_key'], $requests['gateway_api_url']);
         }
         $this->getResponse()->setBody(json_encode($publicKeyStatus));
